bc430a1aad2465cc8e4504551d5b05be2f9df330
Misha <mishakl10@ukr.net>
2021-02-11 21:48:51 +0200

    Added the userReducer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { useDispatch } from "react-redux"
 
 import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css'
 
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import Home from "./pages/Home";
-import Header from "./components/nav/Header";
-import RegisterComplete from "./pages/auth/RegisterComplete";
+import Login from "./pages/auth/Login"
+import Register from "./pages/auth/Register"
+import Home from "./pages/Home"
+import Header from "./components/nav/Header"
+import RegisterComplete from "./pages/auth/RegisterComplete"
 
-
+import { auth } from './lib/firebase'
 import * as ROUTES from './constants/routes'
 
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+      const unsubscribe = auth.onAuthStateChanged(async (user) => {
+          if (user) {
+              const idTokenResult = await user.getIdTokenResult()
+
+              dispatch ({
+                  type: 'LOGGED_IN_USER',
+                  payload: {
+                      name: user.email,
+                      token: idTokenResult.token
+                  }
+              })
+          }
+      })
+
+      return () => unsubscribe()
+  }, [])
+
   return (
     <>
         <Header />
