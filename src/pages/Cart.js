e93b438ab84639b49ecc00bd98c9433af7a78d3b
Misha <mishakl10@ukr.net>
2021-03-21 14:39:27 +0200

    The order summary

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,16 +1,52 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { SHOP } from '../constants/routes'
 
 const Cart = () => {
   const { cart, user } = useSelector(state => ({...state}))
   const dispatch = useDispatch()
 
+  const getTotal = () => cart.reduce((current, next) => current + next.count * next.price, 0)
+
+
   return (
-    <div className="container-fluid">
+    <div className="container-fluid pt-2">
       <div className="row">
-        <h4>Cart</h4>
-        {JSON.stringify(cart)}
+        <div className="col-md-8">
+          <h4>Cart ({cart.length} products)</h4>
+          {!cart.length ?
+            <h4>
+              Cart is empty. <Link to={SHOP}>Continue Shopping</Link>
+            </h4>
+            : (
+            "Show cart items"
+          )}
+        </div>
+
+        <div className="col-md-4">
+          <h4>Order Summary</h4>
+          <hr/>
+
+          <p>Products</p>
+          {cart.map((c,i) => (
+            <div key={i}>
+              <p>{c.title} x {c.count} = ${c.count * c.price}</p>
+            </div>
+          ))}
+          <hr/>
+
+          Total: <b>${getTotal()}</b>
+          <hr/>
+
+          {
+            user ? (
+              <button className="btn btn-sm btn-primary mt-2">Proceed to Checkout</button>
+            ) : (
+              <button className="btn btn-sm btn-primary mt-2">Login to Checkout</button>
+            )
+          }
+        </div>
       </div>
     </div>
   )
