094582891e4c215367b26f4f2863b368e9158a27
Misha <mishakl10@ukr.net>
2021-03-22 18:40:49 +0200

    Include stripe

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { loadStripe } from '@stripe/stripe-js/pure'
+import { Elements } from '@stripe/react-stripe-js'
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_KEY)
 
 const Payment = () => {
   return (
     <div className="container p-5 text-center">
-      Payment
+      <h4>Complete your purchase</h4>
+      <Elements stripe={promise}>
+        <div className="col-md-8 offset-md-2">
+
+        </div>
+      </Elements>
     </div>
   )
 }
